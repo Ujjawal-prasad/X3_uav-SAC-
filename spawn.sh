0f8b88fca19aa051e471b697a9cf553729328843
Ujjawal-prasad <116375337+Ujjawal-prasad@users.noreply.github.com>
2025-10-31 01:35:58 +0530

    Update spawn.sh

--- spawn.sh
+++ spawn.sh
@@ -1,2 +1,2 @@
-gnome-terminal -- bash -c "cd && cd /home/flow/reinforcement_learning/gazebo_sim/x3_uav && QT_QPA_PLATFORM_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/qt5/plugins && gz sim model.sdf; exec bash"
+gnome-terminal -- bash -c "cd && cd <path_to_model.sdf> && QT_QPA_PLATFORM_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/qt5/plugins && gz sim model.sdf; exec bash"
 gnome-terminal -- bash -c "ros2 run ros_gz_bridge parameter_bridge --ros-args -p config_file:=ros_gz_bridge.yaml"
